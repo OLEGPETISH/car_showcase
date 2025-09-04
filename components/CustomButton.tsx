82d04cb82410c9577b8d9a03b7ba418425d46d83
Petish <olegpetiw04@gmail.com>
2025-09-04 15:08:53 +0300

    + Cars API

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,17 +2,30 @@
 
 import { CustomButtonProps } from '@/types';
 import React from 'react';
+import Image from 'next/image';
 
-const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles, rightIcon }: CustomButtonProps) => {
   return (
     <button
       type={btnType || "button"}
 className={`flex items-center justify-center font-bold py-2 px-6 min-w-[130px] ${containerStyles}`}
       onClick={handleClick}
     >
-      {title}
+      <span className={`flex-1 ${textStyles}`}>
+        {title}
+        </span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon} 
+            alt="right icon" 
+            fill
+            className="object-contain" 
+          />
+        </div>
+      )}
     </button>
-  );
-};
+  )
+}
 
 export default CustomButton;
